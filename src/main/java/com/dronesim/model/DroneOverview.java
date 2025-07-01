3614ddd432523ff01ba061189ea5328c1f4e88ff
AdrianKatic <katicadrian@gmail.com>
2025-07-01 20:33:11 +0200

    Update DroneOverview.java

--- src/main/java/com/dronesim/model/DroneOverview.java
+++ src/main/java/com/dronesim/model/DroneOverview.java
@@ -6,20 +6,48 @@ public class DroneOverview {
    private final DroneDynamics  dynamics;
 
    public DroneOverview(Drone drone, DroneType type, DroneDynamics dynamics) {
-    this.drone = drone;
-    this.type = type;
-    this.dynamics = dynamics;
+      this.drone = drone;
+      this.type = type;
+      this.dynamics = dynamics;
    }
 
    public Drone getDrone() {
-    return drone;
+      return drone;
    }
 
    public DroneType getType() {
-    return type;
+      return type;
    }
 
    public DroneDynamics getDynamics() {
-    return dynamics;
+      return dynamics;
+   }
+
+   public int getId() {
+      return drone.getId();
+   }
+
+   public String getSerialNumber() {
+      return drone.getSerialNumber();
+   }
+
+   public double getCarriageWeight() {
+      return drone.getCarriage_weight();
+   }
+
+   public String getCarriageType() {
+      return drone.getCarriage_type();
+   }
+
+   public String getStatus() {
+      return dynamics.getStatus();
+   }
+
+   public int getMaxSpeed() {
+      return type.getMax_speed();
+   }
+
+   public String getTypeName() {
+      return type.getTypename();
    }
 }
